94c984c7d472a5c799850a00ee4a69e2dd9f20ae
Jaime Uriel Torres Serrano <juriel@comtor.net>
2025-04-07 19:45:29 -0500

    Update dependencies and API endpoints

--- class-00.py
+++ class-00.py
@@ -1,9 +1,11 @@
 from langchain_ollama import ChatOllama
+from langchain_openai import ChatOpenAI
+from langchain_gemini import ChatGemini
 
-
-ollama_url = "http://192.168.39.136:11434"
-#model_name = "phi3.5"
-model_name = "deepseek-r1"
+#ollama_url = "http://192.168.39.136:11434"
+ollama_url = "http://localhost:11434"
+model_name = "phi3.5"
+#model_name = "deepseek-r1"
 
 chat = ChatOllama(model=model_name,base_url=ollama_url)
 
